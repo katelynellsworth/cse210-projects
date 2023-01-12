7a8fce32acf4a2eb41db7949615499ac71376d0e
Katelyn Ellsworth <ell22022@byui.edu>
2023-01-11 21:22:01 -0700

    Created Guess the number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        string play = "yes";
+
+        while (play == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+            Console.WriteLine(number);
+
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            int guess = int.Parse(userInput);
+
+            while (guess != number)
+            {
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher.");
+                    Console.Write("What is your guess? ");
+                    string newGuess = Console.ReadLine();
+                    guess = int.Parse(newGuess);
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower.");
+                    Console.Write("What is your guess? ");
+                    string newGuess = Console.ReadLine();
+                    guess = int.Parse(newGuess);
+                }
+            }
+            Console.WriteLine("Congradulations! You guessed the number.");
+            Console.Write("Do you want to play again? (yes or no)? ");
+            play = Console.ReadLine();
+        }
     }
 }
