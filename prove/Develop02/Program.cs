9033753fd27be5d571bf6ab5821ac7a671d74d72
Katelyn Ellsworth <ell22022@byui.edu>
2023-01-28 23:51:44 -0700

    removed journal and moved to journal class

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,80 +2,13 @@ using System.IO;
 
 public class Program
 {
-    static List<Entry> journal = new List<Entry>();
 
-    static void CreateNewEntry()
-    {
-        Console.WriteLine("Write entry off of prompt.");
-        PromptGenerator promptGenerator = new PromptGenerator();
-        string prompt = promptGenerator.RandomPrompt();
-
-        Console.WriteLine(prompt);
-        Console.Write("> ");
-        string userInput = Console.ReadLine();
-
-        DateTime theCurrentTime = DateTime.Now;
-        string dateText = theCurrentTime.ToShortDateString();
-
-        Entry entry = new Entry(prompt, userInput, dateText);
-        journal.Add(entry);   
-
-        Console.WriteLine();
-    }
-    static void DisplayJounal(List<Entry> entryList)
-    {
-        Console.WriteLine("Display all entrys in jounal.");
-
-        foreach (Entry entry in entryList)
-        {
-            string prompt = entry.get_prompt();
-            string date = entry.get_date();
-            string response = entry.get_promptResponse();
-
-            Console.WriteLine($"{date} - {prompt}");
-            Console.WriteLine(response);
-        }
-    }
-    static void LoadFile()
-    {
-        Console.WriteLine("Load past journal using file name.");
-
-        Console.WriteLine("What is the filename?");
-        string filename = Console.ReadLine();
-        string[] lines = System.IO.File.ReadAllLines(filename);
-
-        foreach (string line in lines)
-        {
-            string[] parts = line.Split(",");
-
-            string dateText = parts[0];
-            string prompt = parts[1];
-            string response = parts[2];
-        }
-    }
-    static void SaveFile(List<Entry> entryList)
-    {
-        Console.WriteLine("Save journal to file.");
-
-        Console.Write("What is filename? ");
-        string filename = Console.ReadLine();
-
-        using (StreamWriter outputFile = new StreamWriter(filename))
-        {
-           foreach (Entry entry in entryList)
-           {
-                string prompt = entry.get_prompt();
-                string date = entry.get_date();
-                string response = entry.get_promptResponse();
-                
-                outputFile.WriteLine(date, prompt, response);
-           } 
-        }
-    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello to the Journal Program!");
         string reply = "";
+
+        Journal journal = new Journal();
         
         while (reply != "5")
         {
@@ -90,19 +23,19 @@ public class Program
 
             if (reply == "1")
             {
-                CreateNewEntry();
+                journal.CreateNewEntry();
             }
             else if (reply == "2")
             {
-                DisplayJounal(journal);
+                journal.DisplayJounal();
             }
             else if (reply == "3")
             {
-                LoadFile();
+                journal.LoadFile();
             }
             else if (reply == "4")
             {
-                SaveFile(journal);
+                journal.SaveFile();
             }
             else if (reply == "5")
             {
