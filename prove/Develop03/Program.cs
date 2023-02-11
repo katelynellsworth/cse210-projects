797363396ae50a0ca02354a0301522a403a64cc7
Katelyn Ellsworth <ell22022@byui.edu>
2023-02-10 18:03:13 -0700

    deleted greeting message

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Scripture Memorize World!");
-
         Scripture scripture = new Scripture("Ether", "12", "27", "And if men come unto me I will show unto them their weakness. I give unto men weakness that they may be humble; and my grace is sufficient for all men that humble themselves before me; for if they humble themselves before me, and have faith in me, then will I make weak things become strong unto them.");
 
         string userInput = "";
